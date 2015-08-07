911255f7747f2c1a2c0c788f3b1134cc834c8aa3
flanban <johnsfuller@gmail.com>
2015-08-07 17:58:55 -0400

    scroll work

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,19 @@
 $( window ).load(function() {
   // fade out on scroll
   $(window).scroll(function() {
-      var el = $('.hero');    
-      var offset = el.offset();  
-      var opacity = ( (offset.top - el.height() ) / 400 ) * -1;
-      $('.hero').css('opacity', opacity );
+      if ($('.hero').length != 0) {
+        var el = $('.hero');    
+        var offset = el.offset();  
+        var opacity = ( (offset.top - el.height() ) / 400 ) * -1;
+        $('.hero').css('opacity', opacity );
+      }
+      
+      if ($('.hero-text').length != 0) {
+        var el = $('.about-1');    
+        var offset = el.offset();  
+        var opacity = ( (offset.top - el.height() ) / 400 ) * -1;
+        $('.hero-text').css('opacity', opacity );
+      }
   });
 });
 
