c912fd0fa311e7710dce5abbaacccd1b7d5faf8a
flanban <johnsfuller@gmail.com>
2015-08-22 18:36:05 -0400

    fix hamburger boxes

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -88,7 +88,7 @@ $(function(){
       headerHeight = header.outerHeight()
       $(header).animate({
           top: "0"
-      }, 100);
+      }, 150);
       $(this).one("click", hideMobileNav);
   }
   function hideMobileNav() {
