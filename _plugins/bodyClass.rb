7c7082d5a9389971dc01c61367c0ac902594151f
flanban <johnsfuller@gmail.com>
2015-08-26 20:16:39 -0400

    why u no work, footer promo

--- _plugins/bodyClass.rb
+++ _plugins/bodyClass.rb
@@ -1,19 +1,30 @@
 class BodyClassTag < Liquid::Tag  
 
-  def generate_body_class(prefix, id)
-    id = id.gsub(/\.\w*?$/, '').gsub(/[-\/]/, '_').gsub(/^_/, '') # Remove extension from url, replace '-' and '/' with underscore, Remove leading '_'
+  def prefixed_body_class(prefix, id)
+    id = id.gsub(/\.\w*?$/, '') # Remove extension from url
+    id = id.gsub(/[-\/]/, '_')  # Replace '-' and '/' with underscore
+    id = id.gsub(/^_/, '')      # Remove leading '_'
+
+    case prefix
+    when "class"
+      prefix = ""
+    else
+      prefix = "#{prefix}_"
+    end
+
+    "#{prefix}#{id}"
   end
 
   def render(context)
     page = context.environments.first["page"]
     classes = []
 
-    %w[class url ].each do |prop|
+    %w[class url categories tags layout].each do |prop|
       next unless page.has_key?(prop)
       if page[prop].kind_of?(Array)
-        page[prop].each { |proper| classes.push generate_body_class(prop, proper) }
+        page[prop].each { |proper| classes.push prefixed_body_class(prop, proper) }
       else
-        classes.push generate_body_class(prop, page[prop])
+        classes.push prefixed_body_class(prop, page[prop])
       end
     end
 
@@ -21,5 +32,4 @@ class BodyClassTag < Liquid::Tag
   end
 
 end
-
 Liquid::Template.register_tag('body_class', BodyClassTag)
