edf92930ce910866922c84b0d5b5eb4babbebf80
flanban <johnsfuller@gmail.com>
2015-09-15 09:16:53 -0400

    clean svg js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,39 @@
+---
+---
+// remove widows
 $(document).ready(function() {
   $('p, .widowless, h5, .header-1').widowFix();
 });
 
+// Config SVG Icons
+var svgIconConfig = {
+	hamburgerCross : {
+		url : '{{ site.baseurl }}'+'/svg/hamburger.svg',
+		animation : [
+			{ 
+				el : 'path:nth-child(1)', 
+				animProperties : { 
+					from : { val : '{"path" : "m 5.0916789,20.818994 53.8166421,0"}' }, 
+					to : { val : '{"path" : "M 12.972944,50.936147 51.027056,12.882035"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(2)', 
+				animProperties : { 
+					from : { val : '{"transform" : "s1 1", "opacity" : 1}', before : '{"transform" : "s0 0"}' }, 
+					to : { val : '{"opacity" : 0}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(3)', 
+				animProperties : { 
+					from : { val : '{"path" : "m 5.0916788,42.95698 53.8166422,0"}' }, 
+					to : { val : '{"path" : "M 12.972944,12.882035 51.027056,50.936147"}' }
+				} 
+			}
+		]
+	}
+};
 
 
 
